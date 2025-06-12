7d332fbcda8d783ce35359263bce930135e1c44d
Cyberangel2023 <143792812+Cyberangel2023@users.noreply.github.com>
2025-06-12 22:33:06 +0800

    加载图片与显示图片动画实现

--- Table_pet/widget.cpp
+++ Table_pet/widget.cpp
@@ -1,7 +1,58 @@
 #include "widget.h"
+#include <QPaintEvent>
+#include <QPainter>//绘图
+#include <QPixmap>//图片
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-{}
+    : QWidget(parent),
+    timer(new QTimer(this))
+{
+    connect(timer, &QTimer::timeout, [this](){
+        static int index = 0;//记录显示动作的当前图片索引
+        auto paths = this->action_map.value(this->cur_role_act);
+        this->cur_role_pix = paths[index++ % paths.size()];
+        this->update();
+    });
+
+    loadRoleActRes();
+    showActAnimation(RoleAct::Swing_ing);
+}
 
 Widget::~Widget() {}
+
+void Widget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    QPixmap pix;
+    pix.load(this->cur_role_pix.toLocalFile());
+
+    painter.drawPixmap(0, 0, pix);
+}
+
+void Widget::showActAnimation(RoleAct k)
+{
+    timer->stop();
+
+    this->cur_role_act = k;
+
+    timer->start(60);
+}
+
+void Widget::loadRoleActRes()
+{
+    auto addRes = [this](RoleAct k, QString path, int count) {
+        QList<QUrl> paths;
+        char buf[260];
+        for (int i = 0; i <= count; ++i) {
+            memset(buf, 0, sizeof(buf));
+            sprintf_s(buf, path.toStdString().c_str(), i);
+            paths.append(QUrl::fromLocalFile(buf));
+        }
+        action_map.insert(k, paths);
+    };
+
+    addRes(RoleAct::Greet, ":/Greet/resources/action/hs_%d.png", 28);
+    addRes(RoleAct::Swing_ing, ":/Swing_ing/resources/action/qing_%d.png", 32);
+    addRes(RoleAct::Sleep, ":/Sleep/resources/action/sleep_%d.png", 24);
+}
--- Table_pet/widget.h
+++ Table_pet/widget.h
@@ -2,6 +2,25 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QMap>
+#include <QList>
+#include <QUrl>
+#include <QTimer>
+
+class QPaintEvent;
+
+namespace Act {
+    Q_NAMESPACE
+
+enum RoleAct {
+        Greet,
+        Swing_ing,
+        Sleep
+    };
+    Q_ENUM_NS(RoleAct);
+}
+
+    using namespace Act;
 
 class Widget : public QWidget
 {
@@ -10,5 +29,21 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+
+public:
+    void showActAnimation(RoleAct);
+
+private:
+    //加载图片
+    void loadRoleActRes();
+
+private:
+    QMap<RoleAct, QList<QUrl>> action_map;
+    QTimer* timer;
+    RoleAct cur_role_act;
+    QUrl cur_role_pix;
 };
 #endif // WIDGET_H
